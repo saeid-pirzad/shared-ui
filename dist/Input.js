1d60d72e4ead04b35ad4c59da8868c66453b04e2
saeid-pirzad <saeid_pirzad@yahoo.com>
2025-09-22 17:18:31 +0330

    2

--- dist/Input.js
+++ dist/Input.js
@@ -14,8 +14,8 @@ export const Input = (_a) => {
     var { label, error } = _a, props = __rest(_a, ["label", "error"]);
     return (_jsxs("div", { style: { display: 'flex', flexDirection: 'column', marginBottom: '1rem' }, children: [label && _jsx("label", { style: { marginBottom: '0.25rem', fontWeight: 'bold' }, children: label }), _jsx("input", Object.assign({}, props, { style: {
                     padding: '0.5rem',
-                    border: error ? '1px solid red' : '1px solid #ccc',
+                    border: error ? '1px solid red' : '1px solid #f08 ouline-none',
                     borderRadius: '4px',
-                    fontSize: '1rem'
+                    fontSize: '3rem'
                 } })), error && _jsx("span", { style: { color: 'red', marginTop: '0.25rem', fontSize: '0.875rem' }, children: error })] }));
 };
